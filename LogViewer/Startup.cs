cba71b76ddb878b6f5c6fcc48272ba2a004ebd1c
Jejuni <JejuniProgramming@gmail.com>
2018-11-22 11:19:53 +0100

    Changed ClientApp -> LogViewerFrontend

--- LogViewer/Startup.cs
+++ LogViewer/Startup.cs
@@ -36,7 +36,7 @@ namespace LogViewer
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "LogViewerFrontend/dist";
             });
         }
 
@@ -69,7 +69,7 @@ namespace LogViewer
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "LogViewerFrontend";
 
                 if (env.IsDevelopment())
                 {
