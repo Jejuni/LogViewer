9b186ebb8bceb261e587e2ada50c63ae74f4ded8
Daniel Boos <JejuniProgramming@gmail.com>
2018-11-29 16:13:25 +0100

    Tree Parser Change

--- LogViewer/LogViewerFrontend/src/app/Services/tree-parser.service.ts
+++ LogViewer/LogViewerFrontend/src/app/Services/tree-parser.service.ts
@@ -16,8 +16,10 @@ export class TreeParserService {
   public buildTreeFromStringifiedObject(obj: string): Observable<TreeNode[]> {
     let parsedObj;
     try {
-      parsedObj = JSON.parse(obj.replace(/\\/g, '\\$&'));
+      //parsedObj = JSON.parse(obj.replace(/\\/g, '\\$&'));
+      parsedObj = JSON.parse(obj);
     } catch (e) {
+      //console.error(`Given JSON was not able to be parsed. Given error was: [${e}]`)
       throw new Error(`Given JSON was not able to be parsed. Given error was: [${e}]`);
     }
 
